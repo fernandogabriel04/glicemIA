a93e0fe794a927d7ffd28f953fc232aea46d1fee
Fernando Gabriel <fernando.gabriel322@hotmail.com>
2025-11-26 14:12:55 -0300

    feat: PressaoArterial implementando Diagnosticavel, adicionando metodo analisarRisco e RecomendacaoImediata

--- src/main/java/br/com/glicemia/model/vo/PressaoArterial.java
+++ src/main/java/br/com/glicemia/model/vo/PressaoArterial.java
@@ -1,6 +1,8 @@
 package br.com.glicemia.model.vo;
 
 import br.com.glicemia.model.exceptions.ValorInvalidoException;
+import br.com.glicemia.model.exceptions.RiscoEmergenciaException;
+import br.com.glicemia.model.NivelRisco;
 
 public class PressaoArterial extends SinalVital {
 
@@ -13,6 +15,67 @@ public class PressaoArterial extends SinalVital {
         this.sistolica = sistolica;
         this.diastolica = diastolica;
         validar();
+    }
+
+    @Override
+    public NivelRisco analisarRisco() throws RiscoEmergenciaException {
+        NivelRisco risco;
+
+        if (sistolica >= 180 || diastolica >= 120) {
+            risco = NivelRisco.CRITICO;
+            setNivelRisco(risco);
+            throw new RiscoEmergenciaException(
+                "CRISE HIPERTENSIVA detectada: " + sistolica + "/" + diastolica + " mmHg",
+                "CRITICO",
+                "1. Sentar-se confortavelmente e respirar calmamente\n" +
+                "2. NÃO dirigir\n" +
+                "3. Procurar emergência IMEDIATAMENTE\n" +
+                "4. Risco de AVC ou infarto"
+            );
+        }
+
+        if (sistolica < 90 || diastolica < 60) {
+            risco = NivelRisco.ALTO;
+        } else if (sistolica < 120 && diastolica < 80) {
+            risco = NivelRisco.NORMAL;
+        } else if (sistolica < 140 && diastolica < 90) {
+            risco = NivelRisco.ATENCAO;
+        } else if (sistolica < 160 && diastolica < 100) {
+            risco = NivelRisco.ALTO;
+        } else {
+            risco = NivelRisco.ALTO;
+        }
+
+        setNivelRisco(risco);
+        return risco;
+    }
+
+    @Override
+    public String getRecomendacaoImediata() {
+        if (getNivelRisco() == null) {
+            return "Análise de risco não realizada";
+        }
+
+        switch (getNivelRisco()) {
+            case NORMAL:
+                return "Pressão arterial dentro dos valores normais.";
+
+            case ATENCAO:
+                return "Pré-hipertensão detectada. Adote hábitos saudáveis e monitore regularmente.";
+
+            case ALTO:
+                if (sistolica < 90) {
+                    return "Hipotensão detectada. Hidrate-se e evite levantar-se bruscamente. Consulte médico.";
+                } else {
+                    return "Hipertensão detectada. Reduza sal, estresse e consulte seu médico.";
+                }
+
+            case CRITICO:
+                return "EMERGÊNCIA MÉDICA. Procure atendimento imediato.";
+
+            default:
+                return "Status desconhecido";
+        }
     }
 
     @Override
