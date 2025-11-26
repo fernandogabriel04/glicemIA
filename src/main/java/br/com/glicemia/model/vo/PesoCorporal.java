da35decbd62435d0b1991ab84d94711cfcdd3cc5
Fernando Gabriel <fernando.gabriel322@hotmail.com>
2025-11-26 14:13:16 -0300

    feat: PesoCorporal implementando Diagnosticavel, adicionando metodo analisarRisco e RecomendacaoImediata

--- src/main/java/br/com/glicemia/model/vo/PesoCorporal.java
+++ src/main/java/br/com/glicemia/model/vo/PesoCorporal.java
@@ -1,6 +1,8 @@
 package br.com.glicemia.model.vo;
 
 import br.com.glicemia.model.exceptions.ValorInvalidoException;
+import br.com.glicemia.model.exceptions.RiscoEmergenciaException;
+import br.com.glicemia.model.NivelRisco;
 
 public class PesoCorporal extends SinalVital {
 
@@ -15,6 +17,64 @@ public class PesoCorporal extends SinalVital {
         this.altura = altura;
         validar();
         calcularIMC();
+    }
+
+    @Override
+    public NivelRisco analisarRisco() throws RiscoEmergenciaException {
+        if (imc == null) {
+            setNivelRisco(NivelRisco.NORMAL);
+            return NivelRisco.NORMAL;
+        }
+
+        NivelRisco risco;
+
+        if (imc < 16) {
+            risco = NivelRisco.ALTO;
+        } else if (imc < 18.5) {
+            risco = NivelRisco.ATENCAO;
+        } else if (imc < 25) {
+            risco = NivelRisco.NORMAL;
+        } else if (imc < 30) {
+            risco = NivelRisco.ATENCAO;
+        } else if (imc < 35) {
+            risco = NivelRisco.ALTO;
+        } else if (imc < 40) {
+            risco = NivelRisco.ALTO;
+        } else {
+            risco = NivelRisco.ALTO;
+        }
+
+        setNivelRisco(risco);
+        return risco;
+    }
+
+    @Override
+    public String getRecomendacaoImediata() {
+        if (getNivelRisco() == null || imc == null) {
+            return "Análise de risco não realizada";
+        }
+
+        switch (getNivelRisco()) {
+            case NORMAL:
+                return "IMC dentro da faixa saudável. Mantenha hábitos saudáveis.";
+
+            case ATENCAO:
+                if (imc < 18.5) {
+                    return "IMC abaixo do ideal. Consulte nutricionista para ganho de peso saudável.";
+                } else {
+                    return "Sobrepeso detectado. Adote alimentação balanceada e pratique exercícios.";
+                }
+
+            case ALTO:
+                if (imc < 16) {
+                    return "Desnutrição severa. Consulte médico URGENTEMENTE.";
+                } else {
+                    return "Obesidade detectada. Procure acompanhamento médico e nutricional.";
+                }
+
+            default:
+                return "Status desconhecido";
+        }
     }
 
     @Override
